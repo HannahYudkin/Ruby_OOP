6573b5e329d686f53c458578f1eda34a2ebb7659
HannahYudkin <catgirlblue@gmail.com>
2019-09-28 10:11:56 -0400

    added PIN functionality

---
+++ Banking.rb
@@ -0,0 +1,26 @@
+class Account
+    attr_reader :name
+    attr_reader :balance
+    def initalize(name, balance =100)
+      @name = name
+      @balance = balance
+    end
+    
+    private
+    def pin
+      @pin = 1234
+    end
+    
+    def pin_error
+      return "Access denied: incorrect PIN."
+    end
+    
+    public
+    def display_balance(pin_number)
+      if pin_number == pin
+        puts "Balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+  end
